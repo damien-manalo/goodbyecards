1055f730bb9769fbeed6ae5b787b3b24f9cc4797
Diane Manalo <diane.manalo216@gmail.com>
2016-08-24 01:35:28 -0400

    many more changes
    -created index.js
    -updated main.py
    -updates index.html
    -created messages.html

--- main.py
+++ main.py
@@ -27,24 +27,38 @@ jinja_environment = jinja2.Environment(
 
 ## FUNCTIONS and VARS ##
 names = ['Diane', 'Danica', 'Danielle', 'Glenda', 'Leon']
-msg = {}
+sign = {}
 
 def everyone_sign():
 	for i in range(0, len(names)):
 		if i == 0:
-			msg[names[i]] = "Thank you! Your friends: " + ", ".join(names[i+1:len(names)])
+			sign[names[i]] = ", ".join(names[i+1:len(names)])
+		elif i == len(names)-1:
+			sign[names[i]] = ", ".join(names[0:i])
 		else:
-			msg[names[i]] = "Thank you! Your friends: " + ", ".join(names[0:i]) + ", " + ", ".join(names[i+1:len(names)])	
+			sign[names[i]] = ", ".join(names[0:i]) + ", " + ", ".join(names[i+1:len(names)])	
 
 ## HANDLERS ##
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-    	everyone_sign()
-    	template = jinja_environment.get_template('index.html')
-    	for i in range(0,len(names)):	
-    		html = (template.render({'receiver':names[i], 'message':msg[names[i]]}))
-       		self.response.write(html)
+	def get(self):
+		template = jinja_environment.get_template('index.html')
+		html = (template.render())
+		self.response.write(html)
+
+	def post(self):
+		everyone_sign()
+		template1 = jinja_environment.get_template('messages.html')
+		template2 = jinja_environment.get_template('index.html')
+		inputName = self.request.get("name")
+
+		if inputName in names:	    	
+			html = (template1.render({'name':inputName, 'signature':sign[inputName]}))
+			self.response.out.write(html)
+		else:
+			html = (template2.render({'alert':"Name not found!"}))
+			self.response.out.write(html)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler),
 ], debug=True)
